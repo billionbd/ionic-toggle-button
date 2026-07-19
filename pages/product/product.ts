0a24434ae559e5cee2f95f20585efa11dd48b5c7
Abdullah Al Amin <billionbd@gmail.com>
2026-07-19 10:20:51 +0600

    Update product.ts

--- pages/product/product.ts
+++ pages/product/product.ts
@@ -1,1 +1,9 @@
-
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+@Component({
+  selector: 'page-product',
+  templateUrl: 'product.html',
+})
+export class ProductPage {
+  constructor(public navCtrl: NavController) {}
+}
