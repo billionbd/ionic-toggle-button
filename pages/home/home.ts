00c3318002784c8b4521523088fcd1dd9ccd8565
Abdullah Al Amin <billionbd@gmail.com>
2026-07-19 09:43:59 +0600

    Update home.ts

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -1,21 +1,13 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
-
+  constructor(public navCtrl: NavController) {  }
   public isOnline: boolean = false;
-
-
   updateDriverOnlineStatus($event) {
     this.isOnline = !this.isOnline;
   }
-
 }
