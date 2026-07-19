df7119c378c7e98ae6753d58787480447390aef5
Abdullah Al Amin <billionbd@gmail.com>
2026-07-19 10:19:31 +0600

    Update tabs.ts

--- pages/tabs/tabs.ts
+++ pages/tabs/tabs.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
+import { ProductPage } from '../product/product';
 import { HomePage } from '../home/home';
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
   tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab2Root = ProductPage;
+  tab3Root = AboutPage;
+  tab4Root = ContactPage;
   constructor() {  }
 }
